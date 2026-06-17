3e704083e3071e64e4f2d36b7d758f4f35069826
Gerard Falco <gerardfp@users.noreply.github.com>
2026-06-17 18:45:44 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,253 +1,37 @@
-input.onButtonPressed(Button.A, function () {
-    if (led.point(0, 4)) {
-        led.unplot(0, 4)
-        led.plot(1, 4)
-    } else if (led.point(1, 4)) {
-        led.unplot(1, 4)
-        led.plot(2, 4)
-    } else if (led.point(2, 4)) {
-        led.unplot(2, 4)
-        led.plot(0, 4)
-    }
-})
-input.onButtonPressed(Button.AB, function () {
-    if (led.point(0, 4) && led.point(4, 0)) {
-        led.toggle(0, 0)
-        led.toggle(1, 0)
-        led.toggle(0, 1)
-        if (images.createImage(`
-            . . . . #
-            . . . . .
-            . . . . .
-            . . . . .
-            # . . . .
-            `) == led.screenshot()) {
-            basic.showIcon(IconNames.Heart)
-        }
-    }
-    if (led.point(1, 4) && led.point(4, 0)) {
-        led.toggle(1, 0)
-        led.toggle(0, 0)
-        led.toggle(2, 0)
-        led.toggle(1, 1)
-        if (images.createImage(`
-            . . . . #
-            . . . . .
-            . . . . .
-            . . . . .
-            . # . . .
-            `) == led.screenshot()) {
-            basic.showIcon(IconNames.Heart)
-        }
-    }
-    if (led.point(2, 4) && led.point(4, 0)) {
-        led.toggle(2, 0)
-        led.toggle(1, 0)
-        led.toggle(2, 1)
-        if (images.createImage(`
-            . . . . #
-            . . . . .
-            . . . . .
-            . . . . .
-            . . # . .
-            `) == led.screenshot()) {
-            basic.showIcon(IconNames.Heart)
-        }
-    }
-    if (led.point(0, 4) && led.point(4, 1)) {
-        led.toggle(0, 1)
-        led.toggle(0, 0)
-        led.toggle(1, 1)
-        led.toggle(0, 2)
-        if (images.createImage(`
-            . . . . .
-            . . . . #
-            . . . . .
-            . . . . .
-            # . . . .
-            `) == led.screenshot()) {
-            basic.showIcon(IconNames.Heart)
-        }
-    }
-    if (led.point(1, 4) && led.point(4, 1)) {
-        led.toggle(1, 1)
-        led.toggle(0, 1)
-        led.toggle(2, 1)
-        led.toggle(1, 0)
-        led.toggle(1, 2)
-        if (images.createImage(`
-            . . . . .
-            . . . . #
-            . . . . .
-            . . . . .
-            . # . . .
-            `) == led.screenshot()) {
-            basic.showIcon(IconNames.Heart)
-        }
-    }
-    if (led.point(2, 4) && led.point(4, 1)) {
-        led.toggle(2, 1)
-        led.toggle(1, 1)
-        led.toggle(2, 0)
-        led.toggle(2, 2)
-        if (images.createImage(`
-            . . . . .
-            . . . . #
-            . . . . .
-            . . . . .
-            . . # . .
-            `) == led.screenshot()) {
-            basic.showIcon(IconNames.Heart)
-        }
-    }
-    if (led.point(0, 4) && led.point(4, 2)) {
-        led.toggle(0, 2)
-        led.toggle(0, 1)
-        led.toggle(1, 2)
-        if (images.createImage(`
-            . . . . .
-            . . . . .
-            . . . . #
-            . . . . .
-            # . . . .
-            `) == led.screenshot()) {
-            basic.showIcon(IconNames.Heart)
-        }
-    }
-    if (led.point(1, 4) && led.point(4, 2)) {
-        led.toggle(1, 2)
-        led.toggle(0, 2)
-        led.toggle(2, 2)
-        led.toggle(1, 1)
-        if (images.createImage(`
-            . . . . .
-            . . . . .
-            . . . . #
-            . . . . .
-            . # . . .
-            `) == led.screenshot()) {
-            basic.showIcon(IconNames.Heart)
-        }
-    }
-    if (led.point(2, 4) && led.point(4, 2)) {
-        led.toggle(2, 2)
-        led.toggle(1, 2)
-        led.toggle(2, 1)
-        if (images.createImage(`
-            . . . . .
-            . . . . .
-            . . . . #
-            . . . . .
-            . . # . .
-            `) == led.screenshot()) {
-            basic.showIcon(IconNames.Heart)
-        }
-    }
-})
-input.onButtonPressed(Button.B, function () {
-    if (led.point(4, 0)) {
-        led.unplot(4, 0)
-        led.plot(4, 1)
-    } else if (led.point(4, 1)) {
-        led.unplot(4, 1)
-        led.plot(4, 2)
-    } else if (led.point(4, 2)) {
-        led.unplot(4, 2)
-        led.plot(4, 0)
-    }
-})
-input.onGesture(Gesture.Shake, function () {
-    basic.clearScreen()
-    if (Math.randomBoolean()) {
-        led.plot(0, 0)
-    }
-    if (Math.randomBoolean()) {
-        led.plot(1, 0)
-    }
-    if (Math.randomBoolean()) {
-        led.plot(2, 0)
-    }
-    if (Math.randomBoolean()) {
-        led.plot(0, 1)
-    }
-    if (Math.randomBoolean()) {
-        led.plot(1, 1)
-    }
-    if (Math.randomBoolean()) {
-        led.plot(2, 1)
-    }
-    if (Math.randomBoolean()) {
-        led.plot(0, 2)
-    }
-    if (Math.randomBoolean()) {
-        led.plot(1, 2)
-    }
-    if (Math.randomBoolean()) {
-        led.plot(2, 2)
-    }
-    led.plot(4, 0)
-    led.plot(0, 4)
-})
-let iman = convertToText(0)
-// basic.showLeds(`
-// 
-// . . . . #
-// 
-// . . . . .
-// 
-// . . . . .
-// 
-// . . . . .
-// 
-// # . . . .
-// 
-// `)
-basic.clearScreen()
-if (Math.randomBoolean()) {
-    led.plot(0, 0)
-}
-if (Math.randomBoolean()) {
-    led.plot(1, 0)
-}
-if (Math.randomBoolean()) {
-    led.plot(2, 0)
-}
-if (Math.randomBoolean()) {
-    led.plot(0, 1)
-}
-if (Math.randomBoolean()) {
-    led.plot(1, 1)
-}
-if (Math.randomBoolean()) {
-    led.plot(2, 1)
-}
-if (Math.randomBoolean()) {
-    led.plot(0, 2)
-}
-if (Math.randomBoolean()) {
-    led.plot(1, 2)
-}
-if (Math.randomBoolean()) {
-    led.plot(2, 2)
-}
-basic.showLeds(`
-    . . . . #
-    . . . . .
-    . . . . .
-    . . . . .
-    # . . . .
-    `)
-let ima = images.createImage(`
-    . . . . #
-    . . . . .
-    . . # . .
-    . . . . .
-    # . . . .
-    `)
+namespace Screen {
+    
+    export function isOnScreen(i: Image): boolean {
 
-let a = hex(ima)
+        if (i.pixel(0, 0) != led.point(0, 0)) return false;
+        if (i.pixel(1, 0) != led.point(1, 0)) return false;
+        if (i.pixel(2, 0) != led.point(2, 0)) return false;
+        if (i.pixel(3, 0) != led.point(3, 0)) return false;
+        if (i.pixel(4, 0) != led.point(4, 0)) return false;
 
-console.log(Object.keys(ima))
-console.log("")
-led.plot(4, 0)
-led.plot(0, 4)
+        if (i.pixel(0, 1) != led.point(0, 1)) return false;
+        if (i.pixel(1, 1) != led.point(1, 1)) return false;
+        if (i.pixel(2, 1) != led.point(2, 1)) return false;
+        if (i.pixel(3, 1) != led.point(3, 1)) return false;
+        if (i.pixel(4, 1) != led.point(4, 1)) return false;
+
+        if (i.pixel(0, 2) != led.point(0, 2)) return false;
+        if (i.pixel(1, 2) != led.point(1, 2)) return false;
+        if (i.pixel(2, 2) != led.point(2, 2)) return false;
+        if (i.pixel(3, 2) != led.point(3, 2)) return false;
+        if (i.pixel(4, 2) != led.point(4, 2)) return false;
+
+        if (i.pixel(0, 3) != led.point(0, 3)) return false;
+        if (i.pixel(1, 3) != led.point(1, 3)) return false;
+        if (i.pixel(2, 3) != led.point(2, 3)) return false;
+        if (i.pixel(3, 3) != led.point(3, 3)) return false;
+        if (i.pixel(4, 3) != led.point(4, 3)) return false;
+
+        if (i.pixel(0, 4) != led.point(0, 4)) return false;
+        if (i.pixel(1, 4) != led.point(1, 4)) return false;
+        if (i.pixel(2, 4) != led.point(2, 4)) return false;
+        if (i.pixel(3, 4) != led.point(3, 4)) return false;
+        if (i.pixel(4, 4) != led.point(4, 4)) return false;
+
+        return true;
+    }
+}
